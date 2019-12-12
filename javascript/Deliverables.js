3f80188a3c837c06a7eda8032ddc52fe4ae2b3e0
javierb256 <javier.barrera.152@my.csun.edu>
2019-12-11 21:24:35 -0800

    Added Menu screen, Changes to Story
    Added new Menu screen with topics and tables for each one. Made some changes to the story board to say how to set requirements.

--- javascript/Deliverables.js
+++ javascript/Deliverables.js
@@ -1,20 +1,23 @@
 var count = 1;
 function addRow(){
+
     var table = document.getElementById("myTable");
     var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
-    var cell6 = row.insertCell(5);
+    var ID = row.insertCell(0);
+    var Name = row.insertCell(1);
+    var Description = row.insertCell(2);
+    var DueDate = row.insertCell(3);
+    var Requirements = row.insertCell(4);
+    var Tasks = row.insertCell(5);
 
-    cell1.innerHTML = "D-"+count;
-    cell2.innerHTML = "";
-    cell3.innerHTML = "";
-    cell4.innerHTML = "";
-    cell5.innerHTML = "";
-    cell6.innerHTML = "";
+    ID.innerHTML = "D-"+count;
+    Name.innerHTML = "";
+    Description.innerHTML = "";
+    DueDate.innerHTML = ""
+    Requirements.innerHTML = "";
+    Tasks.innerHTML = "";
+
+    
 
     count++;
 }
